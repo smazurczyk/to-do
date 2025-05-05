ebb6dd2c1851f792dfad048dce99c2ae877aa785
smazurczyk <smazurczyk11@gmail.com>
2025-05-05 17:10:25 +0200

    website is now available on "/todo" and "/", implemented animation for checking off tasks and displaying data in a primitive way

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ToDo
 {
@@ -9,8 +11,15 @@ namespace ToDo
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
             WebApplication app = builder.Build();
-            RouteGroupBuilder api = app.MapGroup("/api/todo");
+            RouteGroupBuilder api = app.MapGroup("/todo/api");
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.MapGet("/", async context =>
+            {
+                context.Response.Redirect("http://localhost:3000/todo");
+            });
             api.MapGet("/", TaskController.GetAll); // get all tasks
             api.MapPost("/", TaskController.Add); // add new task
             api.MapPatch("/{id}", TaskController.Edit); // edit task
---
+++ wwwroot/todo/script.js
@@ -0,0 +1,47 @@
+﻿fetch("api").then(response => response.json()).then(json => {
+    console.log(json)
+    const container = document.getElementById("container")
+    json.Data.forEach(element => {
+        const toDoItem = document.createElement("div")
+        toDoItem.classList.add("todo-item")
+        toDoItem.id = element.id
+
+        const item = document.createElement("div")
+        item.classList.add("item") 
+
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.checked = element.Done
+
+        const label = document.createElement("label")
+        label.setAttribute("for", element.id)
+        label.textContent = element.Name
+
+        item.appendChild(checkbox)
+        item.appendChild(label)
+        toDoItem.appendChild(item)
+        container.appendChild(toDoItem)
+
+        if(element.Subtasks) {
+            element.Subtasks.forEach(e => {
+                const subtask = document.createElement("div")
+                subtask.classList.add("subtask")
+
+                const subitem = document.createElement("div")
+                subitem.classList.add("item") 
+        
+                const subcheckbox = document.createElement("input")
+                subcheckbox.type = "checkbox"
+                subcheckbox.checked = e.Done
+        
+                const sublabel = document.createElement("label")
+                sublabel.textContent = e.Name
+                
+                subitem.appendChild(subcheckbox)
+                subitem.appendChild(sublabel)
+                subtask.appendChild(subitem)
+                toDoItem.appendChild(subtask)
+            })
+        }
+    })
+})
